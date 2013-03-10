7633e9cd56a9534c2c2936261db381e84e13cfe9
Alexander Osmanov <grandison@mail.ru>
2013-03-10 16:20:38 +0400

    Dynamic proxy

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -1,8 +1,11 @@
+require 'httparty'
+
 class Proxy
   attr_accessor :host, :port
 
   def initialize
-    @host = "95.79.55.210"
-    @port = "3128"
+    proxy_list = HTTParty.get("http://hideme.ru/api/proxylist.txt?country=RU&type=h&out=plain&lang=ru").body.split("\r\n")
+    proxy = proxy_list.sample
+    @host, @port = proxy.split(":")
   end
 end
