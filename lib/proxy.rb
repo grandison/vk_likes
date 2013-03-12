b722f77c35b8ed0a6c0294c17393ba5e610b756f
Alexander Osmanov <grandison@mail.ru>
2013-03-12 09:34:51 +0400

    Proxy renewal

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -4,9 +4,9 @@ class Proxy
   attr_accessor :host, :port
 
   def initialize
-    # proxy_list = HTTParty.get("http://hideme.ru/api/proxylist.txt?country=RU&type=h&out=plain&lang=ru").body.split("\r\n")
-    # proxy = proxy_list.sample
-    # @host, @port = proxy.split(":")
-    @host, @port = "79.143.86.19", "8000"
+    doc = Nokogiri.HTML(HTTParty.get("http://www.getproxy.jp/en/russia"))
+    proxy_list = doc.css("#mytable .white td[1]").map &:text
+    proxy = proxy_list.sample
+    @host, @port = proxy.split(":")
   end
 end
