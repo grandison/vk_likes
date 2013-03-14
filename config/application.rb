de9884235cbccef76ed1622735aa01a10dedd66e
Alexander Osmanov <grandison@mail.ru>
2013-03-14 21:36:19 +0400

    Падеж

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,6 @@ module VkontakteLikes
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.i18n.default_locale = :ru
   end
 end
