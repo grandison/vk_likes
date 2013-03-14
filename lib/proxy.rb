4c3da18e5abe73dae4dd0365fe05ae10fb36ab89
Alexander Osmanov <grandison@mail.ru>
2013-03-14 23:49:54 +0400

    Check proxies for availability

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'net/ping'
 
 class Proxy
   attr_accessor :host, :port
@@ -6,7 +7,13 @@ class Proxy
   def initialize
     doc = Nokogiri.HTML(HTTParty.get("http://www.getproxy.jp/en/russia"))
     proxy_list = doc.css("#mytable .white td[1]").map &:text
-    proxy = proxy_list.sample
+    proxy = proxy_list.shuffle.find{|proxy| connectable?(proxy) } || proxy_list.sample
+    p proxy
     @host, @port = proxy.split(":")
+  end
+
+  def connectable?(proxy)
+    host, port = proxy.split(':')
+    return Net::Ping::TCP.new(host, port).ping
   end
 end
