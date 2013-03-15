592c0dfad4bfa69c37ce32fba2694c0b03aba7c8
Alexander Osmanov <grandison@mail.ru>
2013-03-15 09:58:10 +0400

    Proxy

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -1,19 +1,28 @@
 require 'httparty'
-require 'net/ping'
+require 'mechanize'
 
 class Proxy
   attr_accessor :host, :port
 
   def initialize
-    doc = Nokogiri.HTML(HTTParty.get("http://www.getproxy.jp/en/russia"))
-    proxy_list = doc.css("#mytable .white td[1]").map &:text
-    proxy = proxy_list.shuffle.find{|proxy| connectable?(proxy) } || proxy_list.sample
-    p proxy
+    @browser = Mechanize.new { |agent|
+      agent.user_agent_alias = 'Mac Safari'
+    }
+    @browser.follow_meta_refresh = true
+    doc          = Nokogiri.HTML(HTTParty.get("http://www.getproxy.jp/en/russia"))
+    proxy_list   = doc.css("#mytable .white td[1]").map &:text
+    proxy        = proxy_list.shuffle.find{|proxy| connectable?(proxy) } || proxy_list.sample
     @host, @port = proxy.split(":")
   end
 
   def connectable?(proxy)
     host, port = proxy.split(':')
-    return Net::Ping::TCP.new(host, port).ping
+    @browser.set_proxy(host, port)
+    begin
+      @browser.get("http://olike.ru")
+    rescue
+      return false
+    end
+    return true
   end
 end
